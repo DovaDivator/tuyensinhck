8b2bf529e0843e110a2b82983734769e3dfed619
DovaDivator <dvttvdthanhan@gmail.com>
2024-08-29 21:28:13 +0700

    32

--- main/index.php
+++ main/index.php
@@ -14,6 +14,8 @@ if (!isset($_SESSION['user_id'])) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Web Tuyển sinh - Trang chủ</title>
+    <link rel="icon" href="../assets/images/logo.png" type="image/png">
+    <link rel="stylesheet" href="../assets/style/style.css">
 </head>
 <body>
     
