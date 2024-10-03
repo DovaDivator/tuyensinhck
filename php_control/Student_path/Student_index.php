2a47d47965ae2a53c5cbe13f6b38375dc275a797
DovaDivator <dvttvdthanhan@gmail.com>
2024-10-03 21:05:56 +0700

    update nhẹ

--- php_control/Student_path/Student_index.php
+++
@@ -1,9 +0,0 @@
-<div class="body_container">
-    <?php //include '../php_control/path_side/DangKyBar.php'; ?>
-    <div class="body_path">
-    <h1>Trang chủ tuyển sinh</h1> 
-    <h3 style="padding-left: 50px;">Danh sách các ngành <font color='red'>HOT</font> hiện tại đang mở!</h3>
-    <div class="table_hold">
-    <?php include '../php_control/path_side/table_nganh.php'; ?>
-<a href="#" >&gt;&gt; Hiển thị toàn bộ danh sách</a>
-</div>
--- php_control/path_side/table_nganh.php
+++ php_control/path_side/table_nganh.php
@@ -1,9 +1,13 @@
-<table class="choose_list">
+<link rel="stylesheet" href="../assets/style/table.css?v=<?php echo filemtime("../assets/style/table.css")?>"> 
+<table class="choose_list" id="top_tuyen_sinh">
     <thead>
         <tr>
             <th id='ma_nganh'>Mã tuyển sinh</th>
             <th id='ten_nganh'>Tên ngành</th>
             <th id='chi_tieu_du_kien'>Chỉ tiêu dự kiến</th>
+            <?php if ($_SESSION['user']['role'] != 'Student') { 
+                echo "<th id='so_luong_dang_ky'>Số lượng đăng ký</th>";
+            }?>
             <th id='to_hop_xet_tuyen'>Tổ hợp xét tuyển</th>
             <th id='thoi_gian'>Thời gian tuyển sinh</th>
             <th id='ghi_chu'>Ghi chú</th>
