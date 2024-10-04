44dd3ceb052982e731ca75d48280bbac1993e8b9
DovaDivator <dvttvdthanhan@gmail.com>
2024-10-04 23:38:38 +0700

    Chỉnh sửa + phân luồng trang chủ (index)

--- php_control/Student_path/Student_index.php
+++ php_control/Student_path/Student_index.php
@@ -1,14 +1,1 @@
-<?php
-
-session_start();
-if (isset($_SESSION['is_logged_in']) && $_SESSION['is_logged_in'] === true) {
-    //echo "<script>alert('welcom');</script>";
-} else {
-    //echo "<script>alert('pls login');</script>";
-    header("Location: ../../main/login.php");
-    exit();
-}
-
-echo "trang chủ cho Sinh vien";
-
-?>
+Trang chủ sinh viên
