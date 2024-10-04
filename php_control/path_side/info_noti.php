b518f290d9ab9cc76e57c91e7c0bebb55a13b34e
DovaDivator <dvttvdthanhan@gmail.com>
2024-10-04 23:36:52 +0700

    notification (demo)

--- php_control/path_side/info_noti.php
+++ php_control/path_side/info_noti.php
@@ -1,45 +1,9 @@
-<div>
-    <div class="listview notification_info">
-        <div class="red_dots mark_notification"></div>
-        <div class="notification_details details">
-            <p class="title"> Thắng duut deet vit ban tum lum ứ ứ ứ</p>
-            <p class="user">Lee Ngọc Thén</p>
-            <p class="date">12/10/2024</p>
-        </div>
-    </div>
-    <div class="listview notification_info">
-        <div class="notification_details details">
-            <p class="title"> Thắng ligmaball</p>
-            <p class="user">Lee Ngọc Thén</p>
-            <p class="date">12/10/2024</p>
-        </div>
-    </div>
-    <div class="listview notification_info">
-        <div class="notification_details details">
-            <p class="title"> Thắng ligmaball</p>
-            <p class="user">Lee Ngọc Thén</p>
-            <p class="date">12/10/2024</p>
-        </div>
-    </div>
-    <div class="listview notification_info">
-        <div class="notification_details details">
-            <p class="title"> Thắng ligmaball</p>
-            <p class="user">Lee Ngọc Thén</p>
-            <p class="date">12/10/2024</p>
-        </div>
-    </div>
-    <div class="listview notification_info">
-        <div class="notification_details details">
-            <p class="title"> Thắng ligmaball</p>
-            <p class="user">Lee Ngọc Thén</p>
-            <p class="date">12/10/2024</p>
-        </div>
-    </div>
-    <div class="listview notification_info">
-        <div class="notification_details details">
-            <p class="title"> Thắng ligmaball</p>
-            <p class="user">Lee Ngọc Thén</p>
-            <p class="date">12/10/2024</p>
-        </div>
-    </div>
+<div class="info_notifi_container text-content">
+    <h2>Thắng duut deet vit ban tum lum ứ ứ ứ</h2>
+    <p class="type">Không xác định</p>
+    <p class="date">Ngày: 12/10/2024</p>
+    <p>Đây là một đoạn văn mẫu để kiểm tra việc xuống dòng. Nếu có từ dài, nó sẽ tự động xuống dòng.
+        Còn nếu nó không xuống được thì <font color="red">TẤT CẢ TẠI THẮNG</font>
+    </p>
+    <h4 class="credit">Từ: Lê Ngọt Thén</h4>
 </div>
--- php_control/path_side/notification.php
+++ php_control/path_side/notification.php
@@ -2,7 +2,8 @@
 <div>
     <div class="linediv notifi">
         <h1>Thông báo</h1>
-        <div class='group_button'>
+        <!-- Nút cho danh sách tin nhắn -->
+        <div class='group_button list_group'>
         <button type="submit" class="icon-button" name="markAsRead">
             <img src="../assets/icon/mark_as_read.png?v=<?php echo filemtime("../assets/icon/mark_as_read.png"); ?>" 
             alt="Đánh dấu đã đọc" title="Đánh dấu đã đọc">            
@@ -12,8 +13,19 @@
             alt="Dọn dẹp" title="Dọn dẹp">            
         </button>
         </div>
+        <!-- Nút cho chi tiết tin nhắn -->
+        <div class='group_button info_group'>
+        <button type="submit" class="icon-button" name="undo">
+            <img src="../assets/icon/undo.png?v=<?php echo filemtime("../assets/icon/undo.png"); ?>" 
+            alt="Quay về" title="Quay về">            
+        </button>
+        <button type="submit" class="icon-button" name="delete">
+            <img src="../assets/icon/delete.png?v=<?php echo filemtime("../assets/icon/delete.png"); ?>" 
+            alt="Xóa tin nhắn" title="Xóa tin nhắn">            
+        </button>
+        </div>
     </div>
     <div class='notifi_content'>
-        <?php include "info_noti.php"; ?>
+        <?php include "list_noti.php"; ?>
     </div>
 </div>
