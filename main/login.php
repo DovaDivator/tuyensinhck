2db6e1ca057b165875f2c3823072f7b0020387e7
Vuong <caubevuong@gmail.com>
2024-10-04 23:18:18 +0700

    add hộp thông báo

--- main/login.php
+++ main/login.php
@@ -1,13 +1,26 @@
+<<<<<<< HEAD
+=======
+<?php
+session_start();
+if (isset($_SESSION['user'])) {
+    header("location: index.php");
+    exit();
+}
+?>
+
+>>>>>>> bfeae6c (add hộp thông báo)
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Web tuyển sinh - Đăng nhập</title>
-    <link rel="icon" href="../assets/images/logo.png?v=<?php echo filemtime('../assets/images/logo.png'); ?>" type="image/png"> 
-    <link rel="stylesheet" href="../assets/style/style.css?v=<?php echo filemtime("../assets/style/style.css")?>">
-    <link rel="stylesheet" href="../assets/style/login.css?v=<?php echo filemtime("../assets/style/login.css")?>">
+    <link rel="icon" href="../assets/images/logo.png?v=<?php echo filemtime('../assets/images/logo.png'); ?>" type="image/png">
+    <link rel="stylesheet" href="../assets/style/style.css?v=<?php echo filemtime("../assets/style/style.css") ?>">
+    <link rel="stylesheet" href="../assets/style/login.css?v=<?php echo filemtime("../assets/style/login.css") ?>">
     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 </head>
 
 <body id="login">
@@ -16,17 +29,22 @@
         <div class="loginAnhDiv">
         </div>
         <div class="loginDivForm">
-            <img src="../assets/images/logo-01.png?v=<?php echo filemtime("../assets/images/logo-01.png")?>" class="uniLogo">
+            <img src="../assets/images/logo-01.png?v=<?php echo filemtime("../assets/images/logo-01.png") ?>" class="uniLogo">
             <h1>Đăng nhập</h1>
             <!-- TODO: Tạm thời chuyển sang index.php sau khi đăng nhập hợp lệ -->
+<<<<<<< HEAD
             <form action="" method="POST">
                 <input type="text" name="username" id="username" placeholder="Tên đăng nhập" required> 
+=======
+            <form action="" method="POST" id=loginForm>
+                <input type="text" name="username" id="username" placeholder="Tên đăng nhập" required>
+>>>>>>> bfeae6c (add hộp thông báo)
                 <br>
                 <input type="password" name="password" id="password" placeholder="Mật khẩu" minlength="6" maxlength="20">
                 <br>
                 <input type="submit" value="Đăng nhập">
             </form>
-        </div> 
+        </div>
     </div>
     <?php
     include '../php_control/backend/LoginCheck.php'
@@ -34,4 +52,75 @@
 
 
 </body>
+<<<<<<< HEAD
 </html>
+=======
+
+</html>
+
+<script>
+    document.getElementById('loginForm').addEventListener('submit', function(e) {
+        e.preventDefault();
+
+        const username = document.getElementById('username').value;
+        const password = document.getElementById('password').value;
+
+        const formData = new FormData();
+        formData.append('username', username);
+        formData.append('password', password);
+
+        fetch('../php_control/backend/LoginCheck.php', {
+                method: 'POST',
+                body: formData,
+                headers: {
+                    'X-Requested-With': 'XMLHttpRequest'
+                }
+            })
+            .then(response => response.text())
+            .then(text => {
+                console.log(text);
+                return JSON.parse(text);
+            })
+            .then(data => {
+                if (data.success) {
+                    Swal.fire({
+                        title: "Đăng nhập thành công.",
+                        width: 600,
+                        padding: "3em",
+                        color: "#716add",
+                        background: "#fff url(https://sweetalert2.github.io/#downloadimages/trees.png)",
+                        html: `
+                        <img src="../assets/animated/nyan-cat.gif" style="width: 100px; display: block; margin: 20px auto 0;" alt="GIF">
+                        `,
+                        backdrop: `
+                                    rgba(0,0,123,0.4)
+                                `
+                    }).then((result) => {
+                        if (result.isConfirmed) {
+                            window.location.href = 'index.php';
+                        }
+                    });
+                    //window.location.href = 'index.php';
+                } else {
+                    // Thông báo tk mk sai 
+                    Swal.fire({
+                        title: "Tên đăng nhập hoặc mật khẩu sai.",
+                        width: 600,
+                        padding: "3em",
+                        color: "#716add",
+                        background: "#fff url(https://sweetalert2.github.io/#downloadimages/trees.png)",
+                        html: `
+                        <p>Vui lòng kiểm tra lại tài khoản mật khẩu.</p>
+                        <img src="../assets/animated/nyan-cat.gif" style="width: 150px; display: block; margin: 20px auto 0;" alt="GIF">
+                        `,
+                        backdrop: `
+                                    rgba(0,0,123,0.4)
+                                `
+                    });
+
+                }
+            })
+            .catch(error => console.error('Error:', error));
+    });
+</script>
+>>>>>>> bfeae6c (add hộp thông báo)
