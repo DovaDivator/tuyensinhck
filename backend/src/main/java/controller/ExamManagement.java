c53523bfd7e1698c445a81f1486795b1f142796a
Nhat Duy <lenhatduy422@gmail.com>
2025-07-16 19:05:36 +0700

    chết cùng project

--- backend/src/main/java/controller/ExamManagement.java
+++ backend/src/main/java/controller/ExamManagement.java
@@ -10,8 +10,10 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import org.json.JSONArray;
 import org.json.JSONObject;
 
+import dao.ExamDataDAO;
 import dao.KyThiManagerDAO;
 import exception.UnauthorizedException;
 import model.UserBasic;
@@ -38,8 +40,90 @@ public class ExamManagement extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
+        response.setContentType("application/json");
+        response.setCharacterEncoding("UTF-8");
+		JSONObject jsonResponse = new JSONObject();
+		DBConnectionMain dbConn = null;
+		Connection conn = null;
+		try {
+			String body = HttpJson.readRequestBody(request);
+			
+//			String token = null;
+//			String authHeader = request.getHeader("Authorization");
+//			if (authHeader != null && authHeader.startsWith("Bearer ")) {
+//				token = authHeader.substring(7);
+//			} else {
+//				throw new UnauthorizedException("Không có Header");
+//			}
+//
+//			response.setContentType("application/json");
+//			response.setCharacterEncoding("UTF-8");
+//
+//			HttpSession session = request.getSession(false);
+//			if (session == null) {
+//				throw new UnauthorizedException("Session không tồn tại hoặc đã hết hạn");
+//			}
+//
+//			UserBasic user = (UserBasic) session.getAttribute("user");
+//			String sessionToken = (String) session.getAttribute("token");
+//			if (user == null || sessionToken == null || !sessionToken.equals(token)) {
+//				throw new UnauthorizedException("User không tồn tại trong session");
+//			}
+			
+			JSONObject json = new JSONObject();
+			try {
+		        json = new JSONObject(body);
+		    } catch (Exception ex) {
+		        // Không thêm json nếu parse lỗi
+		        System.err.println("Lỗi parse JSON: " + ex.getMessage());
+		    }
+			
+			dbConn = new DBConnectionMain();
+			conn = dbConn.getConnection();
+			
+			String type = request.getParameter("type");
+			switch(type) {
+			case "fetch": {
+				boolean fetchdata = KyThiManagerDAO.fetchExamstatus(conn, request.getParameter("id"));
+				jsonResponse.put("data", fetchdata);
+				break;
+			}
+			case "list": {
+				JSONArray examList = ExamDataDAO.fetchExamList(conn, request.getParameter("id"));
+				jsonResponse.put("data", examList);
+				break;
+			}
+		
+			default:
+				throw new Exception("thuộc tính type không hợp lệ " + type);
+			}
+			
+			jsonResponse.put("success", true);
+			jsonResponse.put("message", "Cập nhật thành công!");	
+			
+		} catch (Exception e) {
+			if (e instanceof UnauthorizedException) {
+				response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+			} else {
+				response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+			}
+			e.printStackTrace();
+			jsonResponse.put("success", false);
+			jsonResponse.put("message", e.getMessage());
+		} finally {
+			if (conn != null) {
+				try {
+					conn.close();
+				} catch (Exception e) {
+					System.err.println("Không thể đóng kết nối: " + e.getMessage());
+				}
+			}
+		}
+		response.getWriter().write(jsonResponse.toString());
 	}
+
+	
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
@@ -92,6 +176,11 @@ public class ExamManagement extends HttpServlet {
 			case "fetch": {
 				boolean fetchdata = KyThiManagerDAO.fetchExamstatus(conn, user.getId());
 				jsonResponse.put("data", fetchdata);
+				break;
+			}
+			case "list": {
+				JSONArray examList = ExamDataDAO.fetchExamList(conn, user.getId());
+				jsonResponse.put("data", examList);
 				break;
 			}
 		
---
+++ backend/src/main/java/dao/ExamDataDAO.java
@@ -0,0 +1,97 @@
+package dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+public class ExamDataDAO {
+
+    public static JSONArray fetchExamList(Connection conn, String id) throws SQLException {
+    	String sql =
+    		    "SELECT " +
+    		    "  CASE tc.he " +
+    		    "    WHEN 'dh' THEN 'Đại học' " +
+    		    "    WHEN 'cd' THEN 'Cao đẳng' " +
+    		    "    WHEN 'lt' THEN 'Liên thông' " +
+    		    "    ELSE 'Không xác định' " +
+    		    "  END AS he, " +
+
+    		    "  tc.khoa AS khoa, " +
+
+    		    "  (SELECT mh.name FROM mon_hoc mh WHERE mh.mon_nn = tc.mon_nn) AS monNN, " +
+    		    "  (SELECT mh.name FROM mon_hoc mh WHERE mh.mon_nn = tc.mon_tc) AS monTC, " +
+
+    		    "  JSON_OBJECT( " +
+    		    "    'diemToan', tc.diem_toan, " +
+    		    "    'diemVan', tc.diem_van, " +
+    		    "    'diemTC', tc.diem_tc, " +
+    		    "    'diemNN', tc.diem_nn " +
+    		    "  ) AS ketQua, " +
+
+    		    "  ( " +
+    		    "    SELECT JSON_ARRAYAGG( " +
+    		    "      JSON_OBJECT( " +
+    		    "        'ttMon', dst.mon_thi, " +
+    		    "        'maPhong', dst.ma_phong, " +
+    		    "        'viTri', ( " +
+    		    "          SELECT pt.vi_tri " +
+    		    "          FROM phong_thi pt " +
+    		    "          WHERE pt.ma_phong = dst.ma_phong " +
+    		    "        ), " +
+    		    "        'dateExam', DATE_ADD( " +
+    		    "          dst.ngay_thi, " +
+    		    "          INTERVAL ( " +
+    		    "            SELECT ct.delay_day " +
+    		    "            FROM ca_thi ct " +
+    		    "            WHERE ct.ma_ca = dst.ma_ca " +
+    		    "          ) DAY " +
+    		    "        ), " +
+    		    "        'timeStart', ( " +
+    		    "          SELECT ct.time_start " +
+    		    "          FROM ca_thi ct " +
+    		    "          WHERE ct.ma_ca = dst.ma_ca " +
+    		    "        ), " +
+    		    "        'timeEnd', ( " +
+    		    "          SELECT ct.time_end " +
+    		    "          FROM ca_thi ct " +
+    		    "          WHERE ct.ma_ca = dst.ma_ca " +
+    		    "        ) " +
+    		    "      ) " +
+    		    "    ) " +
+    		    "    FROM ds_thi dst " +
+    		    "    WHERE dst.exam_id = tc.exam_id " +
+    		    "  ) AS dsThi " +
+
+    		    "FROM thi_cu tc WHERE tc.stu_id = ?;";
+    	
+    	System.out.println("id  " + id);
+    	System.out.println(sql);
+    	
+        JSONArray resultArray = new JSONArray();
+
+        try  {
+        	PreparedStatement stmt = conn.prepareStatement(sql);
+        	stmt.setString (1, id);
+            ResultSet rs = stmt.executeQuery();
+            while (rs.next()) {
+                JSONObject json = new JSONObject();
+                json.put("he", rs.getString("he"));
+                json.put("khoa", rs.getString("khoa"));
+                json.put("monNN", rs.getString("monNN"));
+                json.put("monTC", rs.getString("monTC"));
+                json.put("ketQua", new JSONObject(rs.getString("ketQua")));
+                json.put("dsThi", rs.getString("dsThi") != null ? new JSONArray(rs.getString("dsThi")) : new JSONArray());
+                resultArray.put(json);
+            }
+        } catch( SQLException ex ) {
+        	ex.printStackTrace();
+        	throw ex;
+        }
+
+        return resultArray;
+    }
+}
